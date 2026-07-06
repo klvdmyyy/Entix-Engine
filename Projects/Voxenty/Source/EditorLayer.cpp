825ec702f299db803031006359de8be7834ec340
Dmitry <klementievd08@yandex.ru>
2026-07-06 18:24:42 +0400

    Some changes
    diff --git a/Projects/Voxenty/Source/EditorLayer.cpp b/Projects/Voxenty/Source/EditorLayer.cpp
    index 7d74e52..4e34ff8 100644
    --- a/Projects/Voxenty/Source/EditorLayer.cpp
    +++ b/Projects/Voxenty/Source/EditorLayer.cpp
    @@ -9,6 +9,9 @@ EditorLayer::EditorLayer()
    
     void EditorLayer::OnRender()
     {
    +    // Render the scene
    +    Application::Get().GetCurrentScene().OnRender();
    +
         bool show = true;
         ImGui::ShowDemoWindow(&show);
     }
    \ No newline at end of file

--- Projects/Voxenty/Source/EditorLayer.cpp
+++ Projects/Voxenty/Source/EditorLayer.cpp
@@ -9,6 +9,9 @@ EditorLayer::EditorLayer()
 
 void EditorLayer::OnRender()
 {
+    // Render the scene
+    Application::Get().GetCurrentScene().OnRender();
+
     bool show = true;
     ImGui::ShowDemoWindow(&show);
 }
