eaf1e18b1775ba2f5e4d538147eb231258fb9a57
Dmitry <klementievd08@yandex.ru>
2026-08-07 10:50:15 +0400

    smth

--- Sandbox/Source/Sandbox.cpp
+++ Sandbox/Source/Sandbox.cpp
@@ -6,42 +6,11 @@
 
 namespace Entix
 {
-    struct SerTest
-    {
-        Uint32 first;
-        Int32 second;
-        String third;
-
-        void Serialize(JsonArchive& ar)
-        {
-            ar & AField("first", first)
-               & AField("second", second)
-               & AField("third", third);
-        }
-
-        static SerTest Deserialize(JsonArchive& ar)
-        {
-            SerTest res;
-
-            ar & AField("first", res.first)
-               & AField("second", res.second)
-               & AField("third", res.third);
-
-            return res;
-        }
-    };
-
     class SandboxApp : public Application
     {
     public:
         SandboxApp()
         {
-            IO::FileStream stream("ArchiveTesting.json", IO::StreamMode::Read);
-            JsonArchive ar(stream);
-
-            auto serTest = SerTest::Deserialize(ar);
-
-            EX_LOG(LogTemp, Info, "first: {} second: {} third: {}", serTest.first, serTest.second, serTest.third);
         }
     };
 
