a13bb81ff911c6d7bc36666788be468139792d2e
Dmitry <klementievd08@yandex.ru>
2026-08-10 17:47:50 +0400

    Some change

--- CMake/third-party.cmake
+++ CMake/third-party.cmake
@@ -46,3 +46,46 @@ macro(target_link_slang TARGET)
         target_link_libraries(${TARGET} PRIVATE slang::slang)
     endif()
 endmacro()
+
+#####################################################
+################# Fetched Libraries #################
+
+include(FetchContent)
+
+FetchContent_Declare(
+  EnTT
+  GIT_REPOSITORY https://github.com/skypjack/entt
+  GIT_TAG v3.16.0
+  OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_MakeAvailable(EnTT)
+
+FetchContent_Declare(
+  glm
+  GIT_REPOSITORY https://github.com/g-truc/glm
+  GIT_TAG 1.0.3
+  OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_MakeAvailable(glm)
+
+FetchContent_Declare(
+  Tracy
+  GIT_REPOSITORY https://github.com/wolfpld/tracy
+  GIT_TAG bf21d8ea115318e32ea0d97d2b4fbcb9f5773774
+  OVERRIDE_FIND_PACKAGE
+)
+
+set(TRACY_ON_DEMAND OFF CACHE BOOL "" FORCE)
+set(TRACY_STATIC OFF CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(Tracy)
+
+FetchContent_Declare(
+  nlohmann_json
+  GIT_REPOSITORY https://github.com/nlohmann/json
+  GIT_TAG v3.12.0
+  OVERRIDE_FIND_PACKAGE
+)
+
+FetchContent_MakeAvailable(nlohmann_json)
